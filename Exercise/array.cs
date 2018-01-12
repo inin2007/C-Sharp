8801d1eb7eae87c2980404d5a3f9195a96b7e625
Igor Shcherbinin <inin097@gmail.com>
2018-01-12 11:45:27 -0500

    Create array.cs

---
+++ Exercise/array.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Tutorial
+{
+   public static void Main()
+   {
+	   string[] fruits = new string[] {"apple","banana","orange"};
+
+        
+
+      // test code
+      Console.WriteLine(fruits[0]);
+      Console.WriteLine(fruits[1]);
+      Console.WriteLine(fruits[2]);
+   }
+}
