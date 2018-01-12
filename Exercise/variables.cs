2d3edffa0c32ab770864552a102f5c8ecde989eb
Igor Shcherbinin <inin097@gmail.com>
2018-01-12 11:55:14 -0500

    Create variables.cs

---
+++ Exercise/variables.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Tutorial
+{
+   public static void Main()
+   {
+      string productName = "TV";
+      int productYear = 2012;
+      double productPrice = 279.99;
+
+      Console.WriteLine("productName: " + productName);
+      Console.WriteLine("productYear: " + productYear);
+      Console.WriteLine("productPrice: " + productPrice);
+
+   }
+}
