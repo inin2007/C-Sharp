a36e1d26fb202099c0bffd8b1fb267c2d52b5ea0
Igor Shcherbinin <inin097@gmail.com>
2018-01-15 15:24:00 -0500

    Create dictionaries.cs

---
+++ Exercises/dictionaries.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+public class Hello
+{
+    public static void Main()
+    {
+        Dictionary<string, long> inventory = new Dictionary<string, long>();
+	    inventory.Add("apple", 3);
+		inventory.Add("orange", 5);
+		inventory.Add("banana", 2);
+
+        Console.WriteLine(inventory["apple"]);
+        Console.WriteLine(inventory["orange"]);
+        Console.WriteLine(inventory["banana"]);
+
+    }
+}
