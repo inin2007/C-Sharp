7d4257ba39f50377cf0f628f2bb262dd857b7eaf
Igor Shcherbinin <inin097@gmail.com>
2018-01-15 14:46:32 -0500

    Create lists.cs

---
+++ Exercises/lists.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+public class Hello
+{
+    public static void Main()
+    {
+
+		List<int> primeNumbers = new List<int>();
+		int[] array = new int[] {2,3,5,7,11};
+		primeNumbers.AddRange(array);
+		
+        Console.WriteLine(primeNumbers.Count);
+        Console.WriteLine(primeNumbers[0]);
+        Console.WriteLine(primeNumbers[1]);
+        Console.WriteLine(primeNumbers[2]);
+        Console.WriteLine(primeNumbers[3]);
+        Console.WriteLine(primeNumbers[4]);
+
+    }
+}
