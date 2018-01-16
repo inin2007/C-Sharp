6a69c9a60b0149a403430b2cb917a4213c191e7a
Igor Shcherbinin <inin097@gmail.com>
2018-01-16 11:37:50 -0500

    Create whileLoops.cs

---
+++ Exercises/whileLoops.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Functions
+{
+    public static void Main()
+    {
+        string x = "Hi";
+        int n = 10;
+
+		int i = 0;
+        while(i<n)
+		
+	    
+		{
+		Console.WriteLine(x);
+		i++;
+		}
+    }
+
+}
