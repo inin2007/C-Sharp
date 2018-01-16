d7bbd3724b06bc1c1e1f5527ae426b2b62063b48
Igor Shcherbinin <inin097@gmail.com>
2018-01-16 10:36:26 -0500

    Create strings.cs

---
+++ Exercises/strings.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Strings
+{
+    public static void Main()
+    {
+        string firstName = "John";
+        string lastName = "Doe";
+        int age = 27;
+
+        
+        string sentence = firstName +" "+ lastName +" is "+ age +" years old.";
+
+        Console.WriteLine(sentence);
+
+    }
+}
