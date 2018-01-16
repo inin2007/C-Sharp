9d16061cecc53435f02a49de681f9b77e6e545b2
Igor Shcherbinin <inin097@gmail.com>
2018-01-16 11:24:21 -0500

    Create forLoops.cs

---
+++ Exercises/forLoops.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Functions
+{
+    public static void Main()
+  {
+        string x = "Hi";
+        int n = 10;
+		
+		for(int i =0; i < n; i++)
+		{		
+        Console.WriteLine(x);
+		}
+  }
+}
