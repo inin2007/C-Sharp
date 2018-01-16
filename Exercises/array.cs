1daa2a1be63446b811cdbbfd7213c3711782ff16
Igor Shcherbinin <inin097@gmail.com>
2018-01-16 10:22:22 -0500

    Update array.cs

--- Exercises/array.cs
+++ Exercises/array.cs
@@ -6,9 +6,6 @@ public class Tutorial
    {
 	   string[] fruits = new string[] {"apple","banana","orange"};
 
-        
-
-      // test code
       Console.WriteLine(fruits[0]);
       Console.WriteLine(fruits[1]);
       Console.WriteLine(fruits[2]);
