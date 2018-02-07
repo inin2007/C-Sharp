2d817fa2a3bf5855b6b9ffa046c88260d6e1fb85
Igor Shcherbinin <inin097@gmail.com>
2018-02-07 14:41:53 -0500

    Update main.cs

--- w3resource/Basic Exercises 6/main.cs
+++ w3resource/Basic Exercises 6/main.cs
@@ -5,7 +5,7 @@ public class Exercise6
         int x = 2;
         int y = 3;
         int j = 6;
-        int h = 2*3*6;
+        int h = x*y*j;
          System.Console.WriteLine(x +"x"+ y +"x"+j+"="+h); 
          //or
          //System.Console.WriteLine("{x} x {y} x {j} = {h}",x, y, j, h);
