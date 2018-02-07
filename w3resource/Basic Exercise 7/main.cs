2c35de4091ccd84c9ef39cbb502fd27cd5e7cf8b
Igor Shcherbinin <inin097@gmail.com>
2018-02-07 16:01:38 -0500

    Update main.cs

--- w3resource/Basic Exercise 7/main.cs
+++ w3resource/Basic Exercise 7/main.cs
@@ -1,1 +1,23 @@
-we
+public class Exercise7
+{
+    public static void Main( )
+    {
+        int x = 25;
+        int y = 4;
+       // int j = 6;
+        int h = x+y;
+        int q = x-y;
+        int w = x*y;
+        int e = x/y;
+        int t = x%y;
+         //System.Console.WriteLine(x +"x"+ y +"x"+j+"="+h);
+           System.Console.WriteLine("{0} x {1} = {2}",x, y, h);
+           System.Console.WriteLine("{0} x {1} = {2}",x, y, q);
+           System.Console.WriteLine("{0} x {1} = {2}",x, y, w);
+           System.Console.WriteLine("{0} x {1} = {2}",x, y, e);
+           System.Console.WriteLine("{0} x {1} = {2}",x, y, t);
+    }
+}
+
+------------------------------------------------------------------------------------
+
