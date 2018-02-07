38ff8b914f076f83a4860635cd030e0b9e61e0eb
Igor Shcherbinin <inin097@gmail.com>
2018-02-07 14:27:21 -0500

    Create main.cs

---
+++ w3resource/Basic Exercises 6/main.cs
@@ -0,0 +1,14 @@
+public class Exercise6
+{
+    public static void Main( )
+    {
+        int x = 2;
+        int y = 3;
+        int j = 6;
+        int h = 2*3*6;
+         System.Console.WriteLine(x +"x"+ y +"x"+j+"="+h); 
+         //or
+         //System.Console.WriteLine("{x} x {y} x {j} = {h}",x, y, j, h);
+  
+    }
+}
