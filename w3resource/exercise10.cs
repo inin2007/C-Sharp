d3db1ead62648b7c28b48af7cff28dc6fb07a6b3
Igor Shcherbinin <inin097@gmail.com>
2018-02-08 14:13:54 -0500

    Create exercise10.cs

---
+++ w3resource/exercise10.cs
@@ -0,0 +1,10 @@
+public class Exercise9
+{
+    public static void Main( )
+    {
+       int x = 5;
+       int y = 6;
+       int z = 7;
+         System.Console.WriteLine("Results of specified numbers {0}, {1} and {2}, (x+y).z is {3} and x.y + y.z is {4}", x, y, z, (x+y)*z, x*y+y*z);
+    }
+}
