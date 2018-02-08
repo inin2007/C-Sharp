694f11f3308b935bd51d5e45ae6fd0e3bd4c4efb
Igor Shcherbinin <inin097@gmail.com>
2018-02-08 11:43:11 -0500

    Create exercise8.cs

---
+++ w3resource/exercise8.cs
@@ -0,0 +1,13 @@
+public class Exercise8
+{
+    public static void Main( )
+    {
+        int x = 5;
+        int y = 0;
+        while (y < 11)
+         {
+         System.Console.WriteLine("{0} * {1} = {2}", x, y, x*y); 
+         y++;
+         }
+    }
+}
