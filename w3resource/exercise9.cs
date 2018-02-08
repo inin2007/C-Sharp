db5556a478026aba7c2190fe4a21a48b39101505
Igor Shcherbinin <inin097@gmail.com>
2018-02-08 12:47:28 -0500

    Create exercise9.cs

---
+++ w3resource/exercise9.cs
@@ -0,0 +1,12 @@
+public class Exercise9
+{
+    public static void Main( )
+    {
+       int x = 10;
+       int y = 15;
+       int q = 20;
+       int w = 30;
+       int e = (x+y+q+w)/4;
+         System.Console.WriteLine("The average of 10, 15, 20, 30 is: {0}", e);
+    }
+}
