1ee5153ec4a680bd6af3304172262e13c3567be2
Igor Shcherbinin <inin097@gmail.com>
2018-02-09 12:58:26 -0500

    Create exercise12.cs

---
+++ w3resource/exercise12.cs
@@ -0,0 +1,11 @@
+public class Exercise1
+{
+    public static void Main( )
+    {
+        int x = 25;
+         System.Console.WriteLine("{0} {0} {0} {0}", x);
+         System.Console.WriteLine("{0}{0}{0}{0}", x);
+         System.Console.WriteLine("{0} {0} {0} {0}", x);
+         System.Console.WriteLine("{0}{0}{0}{0}", x);
+    }
+}
