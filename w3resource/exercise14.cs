b96831b1dbfd5f9892cf4dd39a18e384277ab980
Igor Shcherbinin <inin097@gmail.com>
2018-02-09 13:22:10 -0500

    Create exercise14.cs

---
+++ w3resource/exercise14.cs
@@ -0,0 +1,11 @@
+public class Exercise1
+{
+    public static void Main( )
+    {
+        int celsius = 30;
+        int kelvin = celsius + 273;
+        int fahrenheit = (celsius * 9 / 5) + 32;
+         System.Console.WriteLine("Kelvin = {0}", kelvin);
+         System.Console.WriteLine("Fahrenheit = {0}", fahrenheit);
+    }
+}
