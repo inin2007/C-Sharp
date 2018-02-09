08efa0c707a08599ab5445a6bc056f132f3d53dc
Igor Shcherbinin <inin097@gmail.com>
2018-02-09 13:10:21 -0500

    Create exercise13.cs

---
+++ w3resource/exercise13.cs
@@ -0,0 +1,13 @@
+public class Exercise1
+{
+    public static void Main( )
+    {
+        int x = 5;
+         System.Console.WriteLine("{0}{0}{0}", x);
+         System.Console.WriteLine("{0} {0}", x);
+         System.Console.WriteLine("{0} {0}", x);
+         System.Console.WriteLine("{0} {0}", x);
+         System.Console.WriteLine("{0}{0}{0}", x);
+
+    }
+}
