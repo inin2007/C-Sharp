91530bce1a96677d27769543aaf8021a07848f43
Igor Shcherbinin <inin097@gmail.com>
2018-02-13 15:04:13 -0500

    Create exercise18.cs

---
+++ w3resource/exercise18.cs
@@ -0,0 +1,8 @@
+public class Exercise18 {
+  static void Main()
+        {
+           int x = -5;
+           int y = 25;
+           System.Console.WriteLine((x < 0 && y > 0) || (x > 0 && y < 0));
+        }
+}
