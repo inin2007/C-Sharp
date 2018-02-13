be60b7b3171c65f22f1105acf855bee75b97bb26
Igor Shcherbinin <inin097@gmail.com>
2018-02-13 13:33:53 -0500

    Update exercise17.cs

--- w3resource/exercise17.cs
+++ w3resource/exercise17.cs
@@ -2,11 +2,8 @@ public class Exercise17
 {
     public static void Main( )
     {
-        string x = "The quick brown fox jumps over the lazy dog.";
-        if (x.Length>=1)
-        {
+        var x = "The quick brown fox jumps over the lazy dog.";
         var y = x.Substring(0,1);
         System.Console.WriteLine(y+x+y);
-        }
     }
 }
