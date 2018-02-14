09bf36e058521707e51fb1683008de74f7873a3f
Igor Shcherbinin <inin097@gmail.com>
2018-02-14 13:35:36 -0500

    Create exercise19.cs

---
+++ w3resource/exercise19.cs
@@ -0,0 +1,12 @@
+public class Exercise19 {
+  static void Main()
+        {
+            System.Console.WriteLine(triple(3,2));
+            System.Console.WriteLine(triple(2,5));
+            System.Console.WriteLine(triple(-2,1));          
+        }
+        public static int triple(int a, int b)
+        {
+            return (a + b)*3;
+        }
+}
