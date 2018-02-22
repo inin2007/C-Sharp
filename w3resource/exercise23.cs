94e525a1ecd66b6c9f21e818e52b5fd24fae5d94
Igor Shcherbinin <inin097@gmail.com>
2018-02-22 10:52:53 -0500

    Create exercise23.cs

---
+++ w3resource/exercise23.cs
@@ -0,0 +1,9 @@
+public class Exercise23
+{
+    public static void Main( )
+    {
+        string x = "WRITE A C# SHARP PROGRAM TO DISPLAY THE FOLLOWING PATTERN USING THE ALPHABET.";
+
+         System.Console.WriteLine(x.ToLower());
+    }
+}
