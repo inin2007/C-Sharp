a9463224a6ea9f62ef42f2bcd64c2f1aa1959c0a
Igor Shcherbinin <inin097@gmail.com>
2018-03-01 16:03:49 -0500

    Create exercise34.cs

---
+++ w3resource/exercise34.cs
@@ -0,0 +1,10 @@
+using System;
+public class Exercise33
+{
+    public static void Main( )
+    {
+        string x = "Hello how are you?";
+         Console.WriteLine("Input a string : "+x);
+         Console.WriteLine(x == "Hello how are you?");
+    }
+}
