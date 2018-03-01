f53a93237b5bb20ebcdf07e9bc443d8708787a83
Igor Shcherbinin <inin097@gmail.com>
2018-03-01 15:44:08 -0500

    Create exercise33.cs

---
+++ w3resource/exercise33.cs
@@ -0,0 +1,13 @@
+using System;
+public class Exercise33
+{
+    public static void Main( )
+    {
+        string x = "Input first integer:";
+        int y = 15;
+         Console.WriteLine(x);
+         Console.WriteLine(y);
+         Console.WriteLine(y % 3 == 0 || y % 7 == 0 );
+
+    }
+}
