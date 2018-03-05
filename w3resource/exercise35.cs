7fef13ba1121766738d306db86b1887f88b49e35
Igor Shcherbinin <inin097@gmail.com>
2018-03-05 11:35:27 -0500

    Create exercise35.cs

---
+++ w3resource/exercise35.cs
@@ -0,0 +1,11 @@
+public class Exercise35
+{
+    public static void Main( )
+    {
+        int x = 75;
+        int y = 250;
+         System.Console.WriteLine("Input a first number(<100): " + x);
+         System.Console.WriteLine("Input a second number(>100): " + y);
+         System.Console.WriteLine(x < 100 && y > 100);
+    }
+}
