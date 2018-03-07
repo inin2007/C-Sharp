3e8fcc50928cd186f044033f2280cf698d0ac3d2
Igor Shcherbinin <inin097@gmail.com>
2018-03-07 14:29:58 -0500

    Update exercise36.cs

--- w3resource/exercise36.cs
+++ w3resource/exercise36.cs
@@ -2,10 +2,10 @@ public class Exercise36
 {
     public static void Main( )
     {
-        int x = 35;
-        int y = 5;
+        int x = -5;
+        int y = 8;
          System.Console.WriteLine("Input a first number:" + x);
          System.Console.WriteLine("Input a second number:" + y);
-         System.Console.WriteLine(((x >= -10 && x <= 10 ))||((y > -10 && y < 10)));
+         System.Console.WriteLine(((x >= -10 && x <= 10 )) || ((y > -10 && y < 10)));
     }
 }
