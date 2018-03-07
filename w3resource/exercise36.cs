472b0188efc3ff555398f99510f3655f293607f2
Igor Shcherbinin <inin097@gmail.com>
2018-03-07 14:27:18 -0500

    Create exercise36.cs

---
+++ w3resource/exercise36.cs
@@ -0,0 +1,11 @@
+public class Exercise36
+{
+    public static void Main( )
+    {
+        int x = 35;
+        int y = 5;
+         System.Console.WriteLine("Input a first number:" + x);
+         System.Console.WriteLine("Input a second number:" + y);
+         System.Console.WriteLine(((x >= -10 && x <= 10 ))||((y > -10 && y < 10)));
+    }
+}
