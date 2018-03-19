f517416bf50e9b217faa90cc31d83c610fada7ff
Igor Shcherbinin <inin097@gmail.com>
2018-03-19 15:01:14 -0400

    Update DT3.cs

--- w3resource/Data Types/DT3.cs
+++ w3resource/Data Types/DT3.cs
@@ -5,16 +5,11 @@ public static void Main()
 {
 string username, password;
 int ctr = 0,dd=0;
-       Console.Write("\n\nCheck username and password :\n");
-	   Console.Write("N.B. : Defaule username and password is : username and password\n");
-       Console.Write("---------------------------------\n"); 
+
 do
 {
-Console.Write("Input a username: ");
-username = Console.ReadLine();
- 
-Console.Write("Input a password: ");
-password = Console.ReadLine();
+username = "Paul";
+password = "Lee";
 if( username == "username" && password == "password" )
 {
     dd=1;
