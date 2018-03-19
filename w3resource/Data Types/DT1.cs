12b67e47903c6416384ffef47b2eb5c71f65a533
Igor Shcherbinin <inin097@gmail.com>
2018-03-19 14:18:41 -0400

    Create DT1.cs

---
+++ w3resource/Data Types/DT1.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+
+public class DT1
+{
+    public static void Main( )
+    {
+        string x = "b ";
+        string y = "a ";
+        string z = "t ";
+         Console.WriteLine((x+y+z).Reverse().ToArray());
+    }
+}
