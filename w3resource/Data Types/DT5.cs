043acd2c79b0092236feb545593d2563187ff340
Igor Shcherbinin <inin097@gmail.com>
2018-03-20 16:13:11 -0400

    Create DT5.cs

---
+++ w3resource/Data Types/DT5.cs
@@ -0,0 +1,11 @@
+public class Exercise1
+{
+    public static void Main( )
+    {
+        double x = 12;
+        double PI = 3.14;
+        double y = 2 * PI * x;
+         System.Console.WriteLine("Input the radius of the circle: "+ x);
+         System.Console.WriteLine("Perimeter of Circle: "+ y);
+    }
+}
