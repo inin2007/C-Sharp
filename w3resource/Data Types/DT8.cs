aed96dbc5e39ac9d64f0c5f766df6b0eddd17196
Igor Shcherbinin <inin097@gmail.com>
2018-03-27 14:35:13 -0400

    Create DT8.cs

---
+++ w3resource/Data Types/DT8.cs
@@ -0,0 +1,10 @@
+public class DT8
+{
+    public static void Main( )
+    {
+        float rad = 2;
+        float pi = 3.1415926535f;
+         System.Console.WriteLine(4 * pi * (rad * 2));         
+         System.Console.WriteLine(4f / 3f * pi * (rad * rad * rad));
+    }
+}
