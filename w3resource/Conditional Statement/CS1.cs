b3701a049ed399f1e5378b5f3bc438e5e2adca0d
Igor Shcherbinin <inin097@gmail.com>
2018-03-28 16:08:24 -0400

    Create CS1.cs

---
+++ w3resource/Conditional Statement/CS1.cs
@@ -0,0 +1,17 @@
+public class CS1
+{
+    public static void Main( )
+    {
+        int num1, num2;
+        num1 = 5;
+        num2 = 5;
+        if (num1 == num2)
+        {
+        System.Console.WriteLine("{0} and {1} are equal", num1, num2);
+        }
+        else
+        {
+        System.Console.WriteLine("{0} and {1} aren't equal", num1, num2);
+        }
+    }
+}
