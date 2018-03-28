dc25ad2c0ec8bea8126c24a17eedb74aeb547573
Igor Shcherbinin <inin097@gmail.com>
2018-03-28 16:13:55 -0400

    Create CS2.cs

---
+++ w3resource/Conditional Statement/CS2.cs
@@ -0,0 +1,16 @@
+public class CS2
+{
+    public static void Main( )
+    {
+        int num;
+        num = 15;
+        if (num % 2 == 0)
+        {
+        System.Console.WriteLine("{0} is an even integer", num);
+        }
+        else
+        {
+        System.Console.WriteLine("{0} is an odd integer", num);
+        }
+    }
+}
