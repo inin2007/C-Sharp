ac4630583fd5c76b1a4cc4652a8ab1a38c7b3949
Igor Shcherbinin <inin097@gmail.com>
2018-04-04 13:58:10 -0400

    Create CS3.cs

---
+++ w3resource/Conditional Statement/CS3.cs
@@ -0,0 +1,16 @@
+public class CS3
+{
+    public static void Main( )
+    {
+         int x = 14;
+         if (x>=0)
+         {
+             System.Console.WriteLine("{0} is a positive number", x);
+         }
+         else
+         {
+             System.Console.WriteLine("{0} is a negative number", x);
+         }
+         
+    }
+}
