52096206b1ed4f7e2d9f1e6ba9fe37e4efdd5a39
Igor Shcherbinin <inin097@gmail.com>
2018-04-04 14:45:48 -0400

    Create CS6.cs

---
+++ w3resource/Conditional Statement/CS6.cs
@@ -0,0 +1,15 @@
+public class CS6
+{
+    public static void Main( )
+    {
+    int m, n;
+     m = -5;
+        if (m > 0)
+          n = 1;
+        else if (m == 0)
+          n = 0;
+        else 
+          n = -1;
+        System.Console.WriteLine("The value of n = {0}", n);
+     }
+}
