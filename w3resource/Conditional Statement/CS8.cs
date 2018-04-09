78cd8630e6928e6cfc637ea79dc2f0c27a536883
Igor Shcherbinin <inin097@gmail.com>
2018-04-09 11:50:07 -0400

    Update CS8.cs

--- w3resource/Conditional Statement/CS8.cs
+++ w3resource/Conditional Statement/CS8.cs
@@ -5,7 +5,7 @@ public class CS8
         int num1, num2, num3;
         num1 = 25;
         num2 = 63;
-        num3 = 110;
+        num3 = 10;
         if ((num1 > num2) && (num1 > num3))
          System.Console.WriteLine("The 1nd Number is the greatest among three");
          else if ((num1 < num2) && (num2 > num3))
